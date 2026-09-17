4b36573a1ca6c9679a50fe73bc4cc95c54229258
Мустафа Муратов <mustafa_2003_2003@mail.ru>
2026-09-18 00:34:36 +0300

    добавил пользовательский интерфейс

---
+++ tasks/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Task
+
+
+def task_list(request):
+    tasks = Task.objects.all().order_by('-created_at')
+
+    return render(
+        request,
+        'tasks/task_list.html',
+        {'tasks': tasks}
+    )
